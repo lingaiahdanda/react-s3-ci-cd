0c9a7503577dc463cfb220101a26566d365f2b86
lingaiahdanda <lingaiahdanda@gmail.com>
2023-01-17 15:53:38 -0600

    minor changes

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello Friends, This is my first time deploying an app to aws/i);
   expect(linkElement).toBeInTheDocument();
 });
