0112b1eeb006b1e903c8bfed7186782df274a29d
lingaiahdanda <lingaiahdanda@gmail.com>
2023-01-30 23:27:18 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Friends, This is my first time deploying an app to aws ok.
+          Hello Friends, This is my first time deploying an app to aws
         </a>
       </header>
     </div>
